325ae6fb79e9f9fc787a60c4fc6fea1f1dc216d6
Fwos <pscs0000@gmail.com>
2025-08-02 09:16:44 +0800

    frameworks

--- src/components/Frameworks.jsx
+++ src/components/Frameworks.jsx
@@ -18,29 +18,28 @@ export function Frameworks() {
 
       const logos = import.meta.glob('../assets/logos/*.svg', { eager: true });
 
+    return (
+      <div className="relative flex h-[20rem] w-full flex-col items-center justify-center scale-[1]">
+        <OrbitingCircles iconSize={40}>
+          {skills.map((skill, index) => {
+            const logo = logos[`../assets/logos/${skill}.svg`];
+            return <Icon key={index} src={logo?.default} />;
+          })}
+        </OrbitingCircles>
 
+        <OrbitingCircles iconSize={30} radius={100} reverse speed={0.5}>
+          {skills
+            .slice()
+            .reverse()
+            .map((skill, index) => {
+              const logo = logos[`../assets/logos/${skill}.svg`];
+              return <Icon key={index} src={logo?.default} />;
+            })}
+        </OrbitingCircles>
+      </div>
+    );
 
-
-      const logo = logos[`../assets/logos/${skill}.svg`];
-
-  return (
-    <div className="relative flex h-[20rem] w-full flex-col 
-    items-center justify-center scale-[1]">
-      <OrbitingCircles iconSize={40}>
-        {skills.map((skill, index) =>(
-        <Icon key={index} src={logo.default}/>
-        
-        ))}
-      </OrbitingCircles>
-      <OrbitingCircles iconSize={30} radius={100} reverse speed={0.5}>
-      {skills.reverse().map((skill, index) =>(
-        <Icon key={index} src={logo.default}/>
-        
-        ))}
-      </OrbitingCircles>
-    </div>
-  );
-}
+    }
 
 const Icon=({src}) => (
 <img src={src} className="rounded-sm -hover:scale-100 duration-200"/>
